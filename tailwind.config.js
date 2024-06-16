9c6e187041ee70af5cdc7b1a4b09bc3202c09766
wangsa19 <dewanggawpw@gmail.com>
2024-06-16 12:22:04 +0700

    finish modal component - tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js,css}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
